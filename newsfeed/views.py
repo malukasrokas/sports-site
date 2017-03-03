ac42c6e26b667632e7f68b3cefecde8627252614
malukasrokas <malukasrokas@gmail.com>
2017-03-03 14:22:28 +0200

    Modified posts to be displayed from database

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'newsfeed/post_list.html', {})
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'newsfeed/post_list.html', {'posts': posts})
