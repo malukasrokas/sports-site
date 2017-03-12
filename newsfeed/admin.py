2407d2cfd4e1c24dfec54e6247a04f15162e3322
Scepanovas <rascal@mailinator.com>
2017-03-12 17:57:57 +0200

    First stepgit commit . from guest side, YOLO commmiting for first timegit commit .

--- newsfeed/admin.py
+++ newsfeed/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import *
 
 # Register models
 admin.site.register(Post)
+admin.site.register(ForumPost)
+admin.site.register(Chat)
+admin.site.register(Comment)
+admin.site.register(Team)
+admin.site.register(Match)
+admin.site.register(Player)
+admin.site.register(Stats)
--- newsfeed/models.py
+++ newsfeed/models.py
@@ -9,3 +9,61 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class ForumPost(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    post = models.TextField()
+    timeStamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class Chat(models.Model):
+    author = models.ForeignKey('auth.User')
+    text = models.CharField(max_length=140)
+    timeStamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.author
+
+class Comment(models.Model):
+    author = models.ForeignKey('auth.User')
+    news = models.ForeignKey(Post, null=True)
+    forumPost = models.ForeignKey(ForumPost, null=True)
+    text = models.TextField()
+    timeStamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.author
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    homeArena = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    height = models.IntegerField()
+    bDay = models.DateTimeField()
+    team = models.ForeignKey(Team)
+
+    def __str__(self):
+        return self.name
+
+class Stats(models.Model):
+    player = models.ForeignKey(Player)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.player
+
+class Match(models.Model):
+    homeTeam = models.ForeignKey(Team, related_name='homeTeam')
+    awayTeam = models.ForeignKey(Team, related_name='awayTeam')
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.homeTeam, self.awayTeam
